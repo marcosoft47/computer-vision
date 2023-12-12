d53632723c19bd7d81285023fad724984aabca7f
marcosoft47 <56490702+marcosoft47@users.noreply.github.com>
2023-12-12 18:28:54 -0300

    Adicionado a classe tecla

--- artimanhas/keyboardanywhere.py
+++ artimanhas/keyboardanywhere.py
@@ -30,34 +30,47 @@ def desenhar(event, x, y, flags, param):
             # margem = np.sum(keyboard)
         holding = False
 
+class Tecla():
+    def __init__(self):
+        self.foi = np.zeros((0,0))
+        self.cor = (255,0,128)
+    def apertou(self):
+        global margem
+        return np.sum(self.foi) > margem
+    def tocar(self):
+        pass
+    def update():
+        pass
+
 if __name__ == '__main__':
 
     fs = fluidsynth.Synth()
     fs.start(driver="pulseaudio")
     sfid = fs.sfload("example.sf2")
     fs.program_select(0, sfid, 0, 0)
+    
 
-    cor = (255,0,128)
     holding = False
     ret0 = ret1 = [0,0]
     margem = 0
     keyboard = np.zeros((1,1))
-    foi = np.zeros((1,1))
+    key = Tecla()
+    key2 = Tecla()
+    cor = (255,0,128)
     cv.namedWindow('colmeia')
     cv.setMouseCallback('colmeia', desenhar)
     while True:
         img = get_video()
         depth = get_depth()
         if not holding:
-            foi = depth[ret0[1]:ret1[1], ret0[0]:ret1[0]]
+            key.foi = depth[ret0[1]:ret1[1], ret0[0]:ret1[0]]
         cv.rectangle(img,(ret0[0],ret0[1]),(ret1[0],ret1[1]),cor,2)
         
         cv.imshow('colmeia', img)
         
         # print(foi)
         # print(np.sum(foi))
-        if np.sum(foi) > margem:
-            
+        if key.apertou():
             fs.noteon(0, 60, 30)
             cor = (0,0,255)
         else:
