35e578271d2ecb32fdb301cbb5bbe752b6bb1ab4
marcosoft47 <56490702+marcosoft47@users.noreply.github.com>
2023-12-13 19:18:21 -0300

    Adicionado um monte de coisa da classe Tecla

--- keyboardAnywhere/keyboardanywhere.py
+++ keyboardAnywhere/keyboardanywhere.py
@@ -3,6 +3,13 @@ import numpy as np
 from freenect import sync_get_video, sync_get_depth
 import fluidsynth
 
+############### TODO ###############
+# Adicionar mais teclas
+# Cara, na boa, será que fluidsynth é o melhor MIDI player de python mesmo? Duvido um pouco mas tá bom
+# Cada tecla ter uma nota diferente
+# Definir automaticamente tamanho maneiro para cada tecla
+# Normalizar valor da margem (i.e. Vai que dá alguma interferenciazinhainha)
+
 #Função para pegar profundidade do Kinect
 def get_depth():
     array,_ = sync_get_depth()
@@ -15,6 +22,7 @@ def get_video():
     array = cv.cvtColor(array,cv.COLOR_RGB2BGR)
     return array
 
+# Desenha retângulo 
 def desenhar(event, x, y, flags, param):
     global ret0, ret1, keyboard, depth, holding, margem
     if event == cv.EVENT_LBUTTONDOWN:
@@ -27,20 +35,32 @@ def desenhar(event, x, y, flags, param):
                 ret0, ret1 = ret1, ret0
             keyboard = depth[ret0[1]:ret1[1], ret0[0]:ret1[0]]
             margem = np.sum(np.add(keyboard,keyboard * 0.015))
-            # margem = np.sum(keyboard)
         holding = False
 
 class Tecla():
     def __init__(self):
-        self.foi = np.zeros((0,0))
+        global ret0, ret1
+        self.area = np.zeros((0,0))
         self.cor = (255,0,128)
+        self.ret = [ret0,ret1]
+    
+    # Verifica se a tecla foi apertada
     def apertou(self):
         global margem
-        return np.sum(self.foi) > margem
+        return np.sum(self.area) > margem
+    
+    # Toca a nota da tecla
     def tocar(self):
         pass
-    def update():
-        pass
+
+    # Mostra o retângulo da tecla no visualizador do OpenCV
+    def render(self):
+        global img
+        cv.rectangle(img,(self.ret[0][0],self.ret[0][1]),(self.ret[1][0],self.ret[1][1]),self.cor,2)
+    
+    # Atualiza o campo de interesse
+    def atualizaArea(self, depth):
+        key.area = depth[self.ret[0][1]:self.ret[1][1], self.ret[0][0]:self.ret[1][0]]
 
 if __name__ == '__main__':
 
@@ -63,19 +83,17 @@ if __name__ == '__main__':
         img = get_video()
         depth = get_depth()
         if not holding:
-            key.foi = depth[ret0[1]:ret1[1], ret0[0]:ret1[0]]
-        cv.rectangle(img,(ret0[0],ret0[1]),(ret1[0],ret1[1]),cor,2)
+            key.ret = [ret0,ret1]
+            key.atualizaArea(depth)
+        key.render()
         
         cv.imshow('colmeia', img)
-        
-        # print(foi)
-        # print(np.sum(foi))
         if key.apertou():
             fs.noteon(0, 60, 30)
-            cor = (0,0,255)
+            key.cor = (0,0,255)
         else:
             fs.noteoff(0,60)
-            cor = (255,0,128)
+            key.cor = (255,0,128)
 
         k = cv.waitKey(1)
         if k == 27:
